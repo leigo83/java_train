80a5a202474adc751b07245d48a6d3b740221b83
leisminimac <leigo83>
2018-09-15 09:02:14 -0700

    next version

---
+++ Main.java
@@ -0,0 +1,27 @@
+package lei.company;
+
+public class Main {
+
+    public int ff = 10;;
+    public static Test FF = new Test ();
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
+
+class Test {
+    public byte value = 10;
+    public int data = 10;
+    public String str = new String ("fsfs");
+    public float f_data = 34.5F;
+
+    Test () {
+        Byte next = value;
+        int lei = 200;
+        System.out.println(str);
+    }
+}
+
+
