a7d86a81f012d6c3f4f053bd9c6022b14c628efa
leisminimac <leigo83>
2018-09-18 09:51:34 -0700

    layoff related

--- Main.java
+++ Main.java
@@ -29,7 +29,7 @@ class Test {
 
         String test = "Qualcomm is going to have another layoff";
         String lake = "lllake";
-        sfdsfds = "new"
+        sfdsfds = "new fsfsfsfs"
 
         System.out.println(str);
     }
