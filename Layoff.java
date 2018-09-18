b151da1e72c1a0a92ff9589702cb5c9ac1acc489
leisminimac <leigo83>
2018-09-18 10:02:53 -0700

    Merge remote-tracking branch 'remotes/origin/layoff' into HEAD
    # Conflicts:
    #	Main.java

--- Layoff.java
+++ Layoff.java
@@ -3,12 +3,6 @@ package lei.company;
 public class Layoff {
 
     public int statistic () {
-        dsgsfkmslfsl
-                fsfsfsfs
-                dsfsfs
-
-                        fefge
-        System.out.println("this is added file");
-        return dd;
+        fsfwfewfwfwfwww
     }
 }
